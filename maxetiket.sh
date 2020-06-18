eb1b156ef07cc60119919a219e0f61583993bc03
wimstockman <wim.stockman@gmail.com>
2020-06-18 13:41:21 +0200

    Update maxetiket.sh
    Rewrote the script to be more compact and make use of exit codes
    the exit code in bash can be read in the var $? which is tested against

--- maxetiket.sh
+++ maxetiket.sh
@@ -1,11 +1,12 @@
-fontsize=12
-echo $fontsize
-pages=$(cat $1  |  iconv -futf-8 -tlatin1 | enscript -MAdress -o"/tmp/v.ps" -B -r -fHelvetica@$fontsize 2>&1 | awk '{print $2}' | grep -o '[0-9]*') 
-echo "pages:-"$pages"-"
-while [ $pages -gt 1 ]
+#!/bin/bash
+# A Script to create Adresse Labels from text files to use with a Dymo Labelwriter 450 
+# If lines are to long to fit on a label the fontsize is reduced to fit without linewraps
+# Written by Wim Stockman
+# Last Updated on 20200618
+fontsize=20
+while cat /tmp/o.txt  | enscript -MAdress -o"/tmp/v.ps" -B -r -fHelvetica@$fontsize 2>&1 | awk '/page/{if ($2 != 1) { exit 3;}} NR > 1 {exit 2;}'; 
+(($? != 0 ));
 do
-fontsize=$(($fontsize - 1))
-echo "fontsize:"$fontsize
-pages=$(cat $1  | iconv -futf-8 -tlatin1 | enscript -MAdress -o"/tmp/v.ps" -B -r -fHelvetica@$fontsize 2>&1 | awk '{print $2}' | grep -o '[0-9]*') 
+	fontsize=$(($fontsize - 1))
+	echo "fontsize:"$fontsize"pts"
 done
-lp -d DYMO /tmp/v.ps
